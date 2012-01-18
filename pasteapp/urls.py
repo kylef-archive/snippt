fefd300f3ee8fd39640db4643398bb8012ccecff
Kyle Fuller <inbox@kylefuller.co.uk>
2012-01-18 17:12:15 +0000

    Use dropbox for static files

--- pasteapp/urls.py
+++ pasteapp/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -19,9 +18,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.STATIC_ROOT,
-    }),
+    url(r'^admin/', include(admin.site.urls))
 )
 
