8db165c707a05defad7575bdebd774a96c6559ab
Kyle Fuller <inbox@kylefuller.co.uk>
2012-01-18 16:23:03 +0000

    Serve static, its only for the CSS

--- pasteapp/urls.py
+++ pasteapp/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -19,4 +20,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    }),
 )
+
