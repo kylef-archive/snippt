54dae6bf42355221aa771b64cae41f09f5b718af
Kyle Fuller <inbox@kylefuller.co.uk>
2012-01-21 19:27:51 +0000

    Install pasteapp

--- pasteapp/settings.py
+++ pasteapp/settings.py
@@ -140,6 +140,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'pasteapp',
     'paste',
     'apikeys',
     'gunicorn',
