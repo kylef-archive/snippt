824d7d5dc4320a6cd4ce91ab02c5ce90416191f5
Jan Harasym <dijit@fsfe.org>
2013-10-20 01:30:53 +0100

    Updated static file location.

--- pasteapp/settings.py
+++ pasteapp/settings.py
@@ -60,14 +60,12 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/srv/snippt/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-if DEBUG:
-    STATIC_URL = '/static/'
-else:
-    STATIC_URL = 'http://dl.dropbox.com/u/2325654/paste/'
+STATIC_URL = '/static/'
+
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
